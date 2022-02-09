ed0c5b4714987b52866ea7d8d511f80b439629f8
Richard Aubrey White <RichardAubrey.White@fhi.no>
2022-02-09 12:10:57 +0100

    onAttach

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,9 +1,16 @@
 #' @import data.table ggplot2
 #' @importFrom magrittr %>%
 .onAttach <- function(libname, pkgname) {
+  version <- tryCatch(
+    utils::packageDescription("splstyle", fields = "Version"),
+    warning = function(w){
+      1
+    }
+  )
+
   packageStartupMessage(paste0(
     "splstyle ",
-    utils::packageDescription("fhidata")$Version,
+    version,
     "\n",
     "https://docs.sykdomspulsen.no/splstyle"
   ))
