560fcd637b29087f0fcfd675e57a7c68abf38773
Celine Victoria Berg-Hansen <CelineVictoria.Berg-Hansen@fhi.no>
2022-03-30 15:28:01 +0200

    including a lot of new parameters!

--- R/epicurve.R
+++ R/epicurve.R
@@ -19,33 +19,113 @@ epicurve <- function(x, granularity_time = "day", ...) {
 #' epicurve(x = d_day, granularity_time = "day")
 #' epicurve(x = d, granularity_time = "isoweek")
 #' @export
-epicurve.default <- function(x, granularity_time = "day", ...) {
+epicurve.default <- function(x,
+                             var_x = "date",
+                             var_y = "cases_n",
+                             breaks_x = splstyle::every_nth(5),
+                             lab_x = NULL,
+                             lab_y = NULL,
+                             lab_main = NULL,
+                             lab_sub = NULL,
+                             format_y = splstyle::format_nor_num_0,
+                             ...) {
 
-  stopifnot(granularity_time %in% c("day", "isoweek"))
+  # lab_y = "Number of reported deaths"
+  # var_y = "N"
+
+  stopifnot(var_x %in% c("date", "isoyearweek"))
   if(FALSE){
     stop("this is an error message")
   }
 
-  # d <- spltidy::generate_test_data() %>% setnames("deaths_n", "cases_n")
-  # d[, date := as.Date("2022-03-01")]
-  # granularity_time <- "day"
-  # granularity_time <- "isoyearweek"
+  # dots <- list(...)
 
-  dots <- list(...)
+  q <- ggplot(x, aes(x = get(var_x), y = get(var_y)))
 
-  if(granularity_time == "day"){
-    q <- ggplot(x, aes(x = date, y = cases_n))
-    q <- q + scale_x_date("Date")
+  if(var_x == "date"){
+    q <- q + scale_x_date(name = lab_x)
   } else{
-    q <- ggplot(d, aes(x = isoyearweek, y = cases_n))
-    q <- q + scale_x_discrete("Isoweek")
+    q <- q + scale_x_discrete(name = lab_x, breaks = breaks_x)
   }
 
   q <- q + geom_col(fill = splstyle::base_color, width = 0.8)
-  q <- q + scale_y_continuous("Number of reported cases",
-                              expand = expansion(mult = c(0, 0.1))
+  q <- q + scale_y_continuous(name = lab_y,
+                              expand = expansion(mult = c(0, 0.1)),
+                              breaks = splstyle::pretty_breaks(5),
+                              labels = format_y
   )
-  q <- q + labs(caption = fhi_caption())
+  # q <- q + labs(caption = fhi_caption())
+  q <- q + labs(title = lab_main, subtitle = lab_sub)
   q <- q + splstyle::theme_fhi_lines_horizontal()
   q
+}
+
+
+
+test_data <- function(var_x = NULL) {
+  set.seed(4)
+  dates <- sample(seq.Date(as.Date("2018-01-01"),
+                           as.Date("2018-03-08"), 1),
+                  20000,
+                  replace = T)
+  d <- expand.grid(
+    location_code = "norge",
+    date = dates
+  )
+  # Convert to data.table
+  setDT(d)
+
+  # print
+  print(d)
+
+  # Convert to data.table
+  setDT(d)
+
+  # aggregate
+  d <- d[,
+         .(
+           N = .N
+         ),
+         keyby = .(
+           location_code,
+           date
+         )
+  ]
+  # aggregated daily dataset that does not contain days with 0 cases
+  print(d)
+
+  # create skeleton
+  skeleton <- data.table(expand.grid(
+    location_code = "norge",
+    date = seq.Date(min(d$date), max(d$date), 1)
+  ))
+
+  # merge the two datasets together
+  d <- merge(d, skeleton, by=c("location_code", "date"), all=T)
+
+  # Fill in 'missing' Ns with 0
+  d[is.na(N), N := 0]
+
+  # Now you have a clean aggregated daily dataset that contains days with 0 cases!
+  print(d)
+
+  if(!is.null(var_x)){
+    # create 3 new variables:
+    d[, isoyearweek := fhi::isoyearweek(date)]
+
+    # aggregate down to weekly level
+    w <- d[,
+           .(
+             N = sum(N)
+           ),
+           keyby = .(
+             location_code,
+             isoyearweek
+           )
+    ]
+    print(w)
+    return(w)
+  }
+
+  return(d)
 }
